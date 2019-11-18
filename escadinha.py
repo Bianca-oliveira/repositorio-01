9670337c728c53d836e415a16e721984f7ad1782
Bianca Ferreira <biancaf2611@gmail.com>
2019-11-18 19:02:27 -0300

    Exercicios

---
+++ escadinha.py
@@ -0,0 +1,6 @@
+numero = int(input('Números de degraus '))
+caractere = input('Escolha o caractere ')
+for item in range(1, numero):
+    print(caractere *item)
+    #print("#"*item)
+    item+=1
---
+++ for.py
@@ -0,0 +1,20 @@
+lista_nomes = ['Bianca', 'Bruna', 'Caroline', 'Hugo']
+
+#i = 0
+
+#while i < len(lista_nomes):
+    #print(lista_nomes[i])
+    #i += 1
+#print(range(0, 5))
+#for nome in lista_nomes:
+    #print(nome)
+#else:
+    #print('Todos os nomes da lista foram imprimidos')
+
+#for item in range(0, 5) :
+    #print(item)
+    #print(lista_nomes[item])
+
+for item in range(0, 11, 2) :
+    print(item)
+
